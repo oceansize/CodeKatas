947711e8a9024c5f8a308eac7969c94e17e06d9d
Roi Driscoll <roi.driscoll@vistaprint.com>
2020-04-06 14:47:56 +0200

    First solution

--- src/FizzBuzz/FizzBuzz.cs
+++ src/FizzBuzz/FizzBuzz.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 
 namespace FizzBuzz
 {
@@ -6,7 +8,32 @@ namespace FizzBuzz
     {
         public string Check(int i)
         {
-            throw new NotImplementedException();
+            if (IsDivisibleByFifteen(i))
+            {
+                return "FizzBuzz";
+            }
+            if (IsDivisibleByThree(i))
+            {
+                return "Fizz";
+            }
+            if (IsDivisibleByFive(i))
+            {
+                return "Buzz";
+            }
+            return i.ToString();
+        }
+
+        private bool IsDivisibleByThree(int number)
+        {
+            return number % 3 == 0;
+        }
+        private bool IsDivisibleByFive(int number)
+        {
+            return number % 5 == 0;
+        }
+        private bool IsDivisibleByFifteen(int number)
+        {
+            return number % 15 == 0;
         }
     }
 }
