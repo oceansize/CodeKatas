d3ce5556817a35517a28a9916a281a721e04ddb8
Daniel Rozo <drozo@vistaprint.com>
2020-04-06 12:23:59 +0200

    Removing Properties and references

---
+++ ChangeToNUnit.ps1
@@ -0,0 +1,11 @@
+$configFiles = Get-ChildItem . *.cs -rec
+foreach ($file in $configFiles)
+{
+    echo $file
+    (Get-Content $file.PSPath) |
+    Foreach-Object { $_ -replace "[TestClass]", "[TestFixture]" } |
+    Foreach-Object { $_ -replace "[TestInitialize]", "[SetUp]" } |
+    Foreach-Object { $_ -replace "[TestCleanup]", "[TearDown]" } |
+    Foreach-Object { $_ -replace "[TestMethod]", "[Test]" } |
+    Set-Content $file.PSPath
+}
